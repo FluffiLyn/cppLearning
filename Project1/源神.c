9959df64da389bd3b66a8478d6aeaf8430a2a353
11817 <11817@AVALI_FLUFFILYN>
2023-08-23 22:49:09 +0800

    添加项目文件。

---
+++ Project1/源神.c
@@ -0,0 +1,18 @@
+//write code
+//1.write main function
+//C语言是从主函数的第一行开始执行的
+//so main function is necessary - like a entrance
+//"int"：函数返回类型，int是整型/整数的意思
+//main：函数名
+//
+//写个代码，print俩字出来
+//printf：库函数-用于在屏幕上打印信息
+//需要引用头文件！！！(stdio.h)
+
+#include <stdio.h>
+int main()
+{
+	printf("傻逼");
+	return 0;
+}
+//There you go.
