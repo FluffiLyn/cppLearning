e2c82b1c96d00722c2961dd316a0d7e8bc8106af
喵 喵 <1181754881@qq.com>
2023-10-26 20:08:44 +0800

    new

--- CppClassAssignment/Assignment4_16.cpp
+++ CppClassAssignment/Assignment4_16.cpp
@@ -5,10 +5,27 @@ int main()
 {
     while(1)
     {
-        cout << "Please enter " 
+        int hour;
+        float rate;
+        
+        cout << "Enter hours worked (-1 to end):"; 
+        cin >> hour;
+        
+        if(hour != -1)
+        {
+            cout << "\nEnter hourly rate of the employee ($00.00):";
+            cin >> rate;
 
+            if(hour <= 40)
+            {
+                cout << "\nSalary is:" << hour * rate << endl;
+            }
+            else
+                cout << "\nSalary is:" << rate * 40 + 1.5 * rate * (hour - 40) << endl;
+        }
+        else
+            break;
     }
 
-
-
+    return 0;
 }
