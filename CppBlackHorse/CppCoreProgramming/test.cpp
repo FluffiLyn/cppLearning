7bc192e1bbd6f48e3e462946f248c4bfa024e427
喵 喵 <1181754881@qq.com>
2024-02-06 13:07:15 +0800

    123

--- CppBlackHorse/CppCoreProgramming/test.cpp
+++ CppBlackHorse/CppCoreProgramming/test.cpp
@@ -1,65 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
-class Animal
-{
-public:
-
-    Animal()
-    {
-        cout << "Animal的构造函数调用" << endl;
-    }
-
-    virtual ~Animal()
-    {
-        cout << "Animal的析构函数调用" << endl;
-    }
-    virtual void speak() = 0;
-};
-
-class Cat :public Animal
-{
-public:
-
-    Cat(string name)
-    {
-        cout << "Cat的构造函数调用" << endl;
-        m_Name = new string(name);
-    }
-    
-    ~Cat()
-    {
-        if(m_Name != NULL)
-        {
-            cout << "Cat的析构函数调用" << endl;
-            delete m_Name;
-            m_Name = NULL;
-        }
-    }
-
-    virtual void speak()
-    {
-        cout << *m_Name << "猫在说话" << endl;
-    }
-
-    string *m_Name;
-};
-
 void test01()
 {
-    Animal * animal = new Cat("Tom");
-    animal->speak();
-    //父类指针在析构的时候，不会调用子类的析构函数
-    //导致子类如果有堆区属性，会出现内存泄漏
-    //所以我们要在Animal类的析构函数前加virtual，使其变为虚析构
-    //如果不理解，可以尝试把virtual去掉，看运行结果有什么不同
-    delete animal;
+    ifstream ifs;
+    ifs.open("test.txt", ios::in);
+
+    //判断文件是否打开成功
+    if(!ifs.is_open())
+    {
+        cout << "文件打开失败" << endl;
+        return;
+    }
+
+    //读数据
+
+    //第一种
+    // char buf[1024] = {0};
+    // while(ifs >> buf)
+    // {
+    //     cout << buf << endl;
+    // }
+
+    // ifs.close();
+
+    //第二种
+    // char buf[1024] = {0};
+    // while (ifs.getline(buf,sizeof(buf)))//一个字符占一个字节
+    // {
+    //     cout << buf << endl;
+    // }
+
+    //第三种
+    string buf;
+    while (getline(ifs, buf))
+    {
+        cout << buf << endl;
+    }
+
 }
 
 int main()
 {
     test01();
-
     return 0;
 }
