265e7ad96bf82bff28e37c4dc6263888e85fae48
FluffiLithium <1181754881@qq.com>
2024-03-28 12:05:24 +0000

    你知道吗，我已经学完了STL容器了

--- CppCoreProgramming/Tests/test.cpp
+++ CppCoreProgramming/Tests/test.cpp
@@ -1,14 +1,7 @@
-// basics/max1.cpp
-#include "max1.hpp"
-#include <iostream>
+#include <stack>
+#include <vector>
 
 int main()
 {
-    int i = 42;
-    std::cout << "max(7,i): " <<::max(7,i) << std::endl;
-
-    double f1 = 3.4, f2 = -6.7;
-    std::cout << "max(f1,f2): " << ::max(f1, f2) << std::endl;
-
-    return 0;
+    stack<int,vector<int>> ;
 }
