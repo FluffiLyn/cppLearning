cc343c4de23f7cb61a6e81a39a16b56c870af016
FluffiLithium <1181754881@qq.com>
2024-04-10 11:45:29 +0000

    1

--- OI/Chapter6_GreedyAlgorithm/test.cpp
+++ OI/Chapter6_GreedyAlgorithm/test.cpp
@@ -1,50 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int moneyType[7] = {1,2,5,10,20,50,100};
-int moneyNum[7] = {2,5,1,3,4,0,4};
-
-int cnt[7] = {0};//每种面值使用的张数
-int total_count;
-
-int greedy(int money)
-{
-    if (money >= 0)
-    {
-        for (int i = 6; i >= 0; i--)
-        {
-            cnt[i] = min(moneyNum[i], money / moneyType[i]);
-            money = money - cnt[i] * moneyType[i];
-        }
-        return 0;
-    }
-    else
-    {
-        return money;
-    }
-}
-
 int main()
 {
-    int money;
-    cout << "请输入金额总数: ";
-    cin >> money;
-
-    if(money < 0)
+    int n = 0, sum = 0, a[100];
+    cin >> n;
+    for(int i = 0; i < n; i++)
     {
-        cout << "Error!" << endl;
-
+        cin >> a[i];
+        sum += a[i];
     }
-    else
-    {
-        greedy(money);
-        for (int j = 6; j>=0; j--)
-        {
-            cout<<moneyType[j]<<"元:"<<cnt[j]<<"张"<<endl;
-        }
 
+    sum /= n;
+    
 
-    }
 
     return 0;
 }
